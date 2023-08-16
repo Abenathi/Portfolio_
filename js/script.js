873d14a0b9a449cb31cefcb16ac79a014516e7b4
Abenathi <cwatiabenathi@gmail.com>
2023-08-16 10:45:21 +0200

    mediaquery

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 //toggle nav bar 
-let menuIcon = document.querySelector('#menu');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
 //Navbar toggle end
 //scroll sections
@@ -19,8 +20,7 @@ window.onscroll = () => {
         let top = window.scrollY; 
         let offset = sec.offsetTop - 100; 
         let height = sec.offsetHeight; 
-        let id = sec.getAttributes('id');
-        alert('now scrolling');
+        let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
@@ -36,4 +36,8 @@ window.onscroll = () => {
     //sticky header
     let header = document.querySelector('header');
     header.classList.toggle('sticky'.window.scrollY > 100);
+
+    //remove toggle icon and navbar  when click navbar links(scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
